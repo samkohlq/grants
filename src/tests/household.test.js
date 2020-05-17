daa450e924521721e92bededdd9306b4920f92e9
Samantha Koh <26067359+samkohlq@users.noreply.github.com>
2020-05-17 15:11:37 +0800

    group createHousehold tests in a block, clean up database after each test runs, close sequelize connection when all tests have run

--- src/tests/household.test.js
+++ src/tests/household.test.js
@@ -2,25 +2,32 @@ import request from "supertest";
 import app from "../app";
 import models from "../db/models";
 
-test("createHousehold endpoint creates new household with housingType 'Landed'", async () => {
-  // create household
-  const response = await request(app)
-    .post("/households/createHousehold")
-    .send({ housingType: "Landed" })
-    .set("Accept", "application/json");
-  // assert that response contains the housingType of the new household
-  expect(response.statusCode).toBe(200);
-  expect(response.body.housingType).toBe("Landed");
+afterEach(async () => {
+  // clean up database after each test is done
+  await models.Household.destroy({ where: {} });
+});
+
+afterAll(() => {
+  // close sequelize connection when all tests are done
   models.sequelize.close();
 });
 
-test("createHousehold endpoint validates against unrecognised housingType and does not create new household", async () => {
-  // create household
-  const response = await request(app)
-    .post("/households/createHousehold")
-    .send({ housingType: "Underground" })
-    .set("Accept", "application/json");
-  // assert that response contains the housingType of the new household
-  expect(response.statusCode).toBe(422);
-  models.sequelize.close();
+describe("createHousehold endpoint validates and creates households", () => {
+  test("createHousehold endpoint creates new household with housingType 'Landed'", async () => {
+    const response = await request(app)
+      .post("/households/createHousehold")
+      .send({ housingType: "Landed" })
+      .set("Accept", "application/json");
+    // assert that response contains the housingType of the new household
+    expect(response.statusCode).toBe(200);
+    expect(response.body.housingType).toBe("Landed");
+  });
+  test("createHousehold endpoint validates against unrecognised housingType and does not create new household", async () => {
+    const response = await request(app)
+      .post("/households/createHousehold")
+      .send({ housingType: "Underground" })
+      .set("Accept", "application/json");
+    // assert that response contains the housingType of the new household
+    expect(response.statusCode).toBe(422);
+  });
 });
