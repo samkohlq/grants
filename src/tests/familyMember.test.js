82c239c54a40c261e209dd575a8940cbb161a5a9
Samantha Koh <26067359+samkohlq@users.noreply.github.com>
2020-05-17 18:30:30 +0800

    in familyMember.test.js, specify expected field values in addFamilyMember response

--- src/tests/familyMember.test.js
+++ src/tests/familyMember.test.js
@@ -4,8 +4,8 @@ import models from "../db/models";
 
 afterEach(async () => {
   // clean up database after each test is done
-  await models.Household.destroy({ where: {} });
-  await models.FamilyMember.destroy({ where: {} });
+  // await models.Household.destroy({ where: {} });
+  // await models.FamilyMember.destroy({ where: {} });
 });
 
 afterAll(() => {
@@ -29,10 +29,22 @@ describe("addFamilyMember endpoint validates and adds a family member to househo
         maritalStatus: "Single",
         occupationType: "Unemployed",
         annualIncome: 100000,
-        birthDate: new Date("2020-05-15 17:01:07.941+08"),
+        birthDate: new Date("2020-05-15").toISOString(),
       })
       .set("Accept", "application/json");
     // assert that response contains the housingType of the new household
+    expect(addFamilyMemberResponse.body.HouseholdId).toBe(
+      createHouseholdResponse.body.id
+    );
+    expect(addFamilyMemberResponse.body.name).toBe("FamilyMemberName");
+    expect(addFamilyMemberResponse.body.gender).toBe("Female");
+    expect(addFamilyMemberResponse.body.maritalStatus).toBe("Single");
+    expect(addFamilyMemberResponse.body.spouseId).toBe(null);
+    expect(addFamilyMemberResponse.body.occupationType).toBe("Unemployed");
+    expect(addFamilyMemberResponse.body.annualIncome).toBe(100000);
+    expect(addFamilyMemberResponse.body.birthDate).toBe(
+      "2020-05-15T00:00:00.000Z"
+    );
     expect(addFamilyMemberResponse.statusCode).toBe(200);
   });
   test("addFamilyMember endpoint validates against unrecognised marital status and does not add new family member", async () => {
